b3c6ee76a9302df11febcdbd6786d35d942a1155
Saad Javiad Alvi <saadjavaidalvi@gmail.com>
2022-03-07 23:05:15 +0500

    clean code

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      // title: 'Flutter Demo',
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
